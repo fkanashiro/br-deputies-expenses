dcb99770111c719b618156dccf2be3be08cdc74d
Felipe Kanashiro <felipekanashiro@live.com>
2025-03-13 01:21:03 -0300

    add csv url retrieve

--- src/download-expenses-csv.py
+++ src/download-expenses-csv.py
@@ -6,11 +6,7 @@ def download_expenses():
     starting_year = 2008
     current_year = datetime.now().year;
 
-    for i in range(starting_year, current_year+1):
+    for i in range(starting_year, current_year + 1):
         print(base_url.format(year = i))
-
-
-    #response = requests.get(base_url)
-    #urlretrieve("https://www.camara.leg.br/cotas/Ano-{starting_year}.csv.zip", "Ano-2008.csv.zip")
     
 download_expenses()
